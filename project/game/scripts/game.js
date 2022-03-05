64f3512915d558874cdb4e1d6034d821f7409a5c
Zhasmina Mihaleva <ZSMihaleva20@codingburgas.bg>
2022-03-06 01:47:38 +0200

    Add functions and comments in js file of the game

--- project/game/scripts/game.js
+++ project/game/scripts/game.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text')); /*elements connected to styles*/
+const choices = Array.from(document.querySelectorAll('.choice-text')); /*all elements connected to styles*/
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
@@ -12,85 +12,85 @@ let availableQuestions = []
 
 let questions = [ /*questions*/
     {
-        question: "What is 2+ 2?",
+        question: "What is 2 + 2?",
         choice1: '2',
         choice2: '4',
         choice3: '21',
         choice4: '17',
-        answer: 2,
+        answer: 2, //second option for answer -- > choice 2: ''
     },
     {
         question: "What is 2 + 1?",
         choice1: '3',
-        choice2: '3',
+        choice2: '5',
         choice3: '21',
         choice4: '17',
-        answer: 1,
+        answer: 1, //first option for answer -- > choice 2: ''
     }
 ]
 
 const scorePoints = 100;
 const maxQuestions = 2;
 
-startGame = () => {
+startGame = () => { // start game function
     questionCounter = 0
     score = 0
-    availableQuestions = [...questions]
+    availableQuestions = [...questions] // to sepate the questions
     getNewQuestion()
 }
 
-getNewQuestion = () => {
-    /*if (availableQuestions.length === 0 || questionCounter > maxQuestions) {
-        localStorage.setItem('mostRecentScore', score)
+getNewQuestion = () => { // getNewQuestion function
+    if (availableQuestions.length === 0 || questionCounter > maxQuestions) // if number of available question is 0 or number of question is bigger than maxQuestions
+    {   
+        localStorage.setItem('mostRecentScore', score) // print out the most recent score and corenspondible score
 
-        return window.location.assign('/end.html')
-    }*/
-
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${maxQuestions}`
-    progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%`
-
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
-
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptionAnswers = true
+        return window.location.assign('/end.html') // end game of the page
+    }
+        questionCounter++ //increment a question counter
+        progressText.innerText = `Question ${questionCounter} of ${maxQuestions}` // Print out the number of question
+        progressBarFull.style.width = `${(questionCounter/maxQuestions) * 100}%` // fulling the progress Bar
+    
+        const questionsIndex = Math.floor(Math.random() * availableQuestions.length) // set up the current question and randomly choose a question
+        currentQuestion = availableQuestions[questionsIndex]
+        question.innerText = currentQuestion.question
+    
+        choices.forEach(choice => { //set up the answers
+            const number = choice.dataset['number']
+            choice.innerText = currentQuestion['choice' + number]
+        })
+    
+        availableQuestions.splice(questionsIndex, 1) //add the incrementation to make available questions and questions index equal
+    
+        //acceptionAnswers = true //the value accept answers for 
 }
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
+choices.forEach(choice => { // function for each choice
+    choice.addEventListener('click', e => { // when is clicked
         if (!acceptionAnswers) return
 
-        acceptionAnswers = false;
+        //acceptionAnswers = false;
         const selectedChoice = e.target
         const selectedAnswer = selectedChoice.dataset['number']
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
-        if (classToApply === 'correct')
+        if (classToApply === 'correct') //incrementing a score
         {
             incrementScore(scorePoints)
         }
 
-        selectedChoice.parentElement.classList.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply) //color of the answer according if it is true or false --> false - red, true - green <--
 
-        setTimeout (() => {
+        setTimeout (() => { //time to move on to the next question
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
         }, 1000)
     })
 })
 
-incrementScore = num => {
+incrementScore = num => { // incrementing the score
     score += num
     scoreText.innerText = score
 }
 
-startGame()
+startGame() //starting the game
