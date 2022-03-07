fb97d328c63b06ea75104db02c74d2c8ba2de51a
Zhasmina Mihaleva <ZSMihaleva20@codingburgas.bg>
2022-03-08 01:57:40 +0200

    Make a responsible navigation bar

--- project/website/scripts/script.js
+++ project/website/scripts/script.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll",function(){
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+const navigation = document.querySelector('nav');
+
+document.querySelector('.toogle').onclick = function() {
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
