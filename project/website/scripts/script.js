53c1f33c9a863c10332faed7d2fc508b594ffb33
Krisi Stojkova <KRStoykova@codingburgas.bg>
2022-03-13 19:07:18 +0200

    Edit the first region

--- project/website/scripts/script.js
+++ project/website/scripts/script.js
@@ -1,8 +1,3 @@
-window.addEventListener("scroll",function(){
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-})
-
 const navigation = document.querySelector('nav');
 
 document.querySelector('.toogle').onclick = function() {
