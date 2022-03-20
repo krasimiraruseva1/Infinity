50c8210821665ed4e36de4082cdd2f990a5ea436
Kristiyana Stoykova <KRStoykova20@codingburgas.bg>
2022-03-20 13:30:29 +0200

    Edit html, java and style files

--- project/website/scripts/index.js
+++ project/website/scripts/index.js
@@ -1,4 +1,19 @@
 window.addEventListener("scroll",function(){
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+    const body = document.querySelector('body');
+ 
+    if(window.scrollY > 30) {
+        if(!body.classList.contains('sticky')) {
+            body.classList.add('sticky');
+            setTimeout(function() {
+                window.scrollTo({
+                    top: 100,
+                    left: 0,
+                    behavior: 'smooth'
+                  });
+            }, 1500);
+        }
+    } else {
+        body.classList.remove('sticky');
+    }
+    
 })
